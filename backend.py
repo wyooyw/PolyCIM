8c56cfc8f792a529d03eb1713c7ab0e287d9a14d
wangyiou <wangyiou12@163.com>
2024-11-14 06:05:32 -0800

    fix backend

--- backend.py
+++ backend.py
@@ -1,5 +1,7 @@
 import islpy as isl
 import os
+import json
+from dataclasses import dataclass
 
 def dump_op_basic_info(op, path):
     n_compute = op.domain.count_val()
@@ -10,12 +12,18 @@ def dump_op_basic_info(op, path):
         f.write(f"access_O: {op.access_O}\n")
         f.write(f"access_W: {op.access_W}\n")
 
+@dataclass
+class ProfileResult:
+    stats: str
+    save_path: int
+
 def backend_compile_and_profile_pass(op_list, save_dir=None):
     assert save_dir is not None
     assert os.path.exists(save_dir), f"{save_dir=}"
     assert os.path.isdir(save_dir), f"{save_dir=}"
     
     backend_compile_cmd_list = []
+    result_list = []
     for idx,op in enumerate(op_list):
         dsl = op.dsl
         save_path_dir = os.path.join(save_dir, f"{idx}")
@@ -29,15 +37,27 @@ def backend_compile_and_profile_pass(op_list, save_dir=None):
         os.system(backend_compile_cmd)
 
         # run simulator to profile
-        # input_path = os.path.join(os.path.abspath(save_path_dir), "final_code.json")
-        # output_path = os.path.join(os.path.abspath(save_path_dir), "stats")
-        # os.makedirs(output_path, exist_ok=True)
-        # config_path = "/home/wangyiou/project/cim_compiler_frontend/playground/config/config.json"
-        # simulator_path = "/home/wangyiou/project/cim_compiler_frontend/playground"
-        # cd_cmd = f"cd {simulator_path}"
-        # run_cmd = f"python utils/simulate_and_stats.py --input {input_path} --output {output_path} --config {config_path}" 
-        # backend_simulator_cmd = f"{cd_cmd} && {run_cmd}"  
+        input_path = os.path.join(os.path.abspath(save_path_dir), "final_code.json")
+        output_path = os.path.join(os.path.abspath(save_path_dir), "stats")
+        os.makedirs(output_path, exist_ok=True)
+        config_path = "/home/wangyiou/project/cim_compiler_frontend/playground/config/config.json"
+        simulator_path = "/home/wangyiou/project/cim_compiler_frontend/playground"
+        cd_cmd = f"cd {simulator_path}"
+        run_cmd = f"python utils/simulate_and_stats.py --input {input_path} --output {output_path} --config {config_path}" 
+        backend_simulator_cmd = f"{cd_cmd} && {run_cmd}"  
         # os.system(backend_simulator_cmd) 
 
         # save op info
-        # dump_op_basic_info(op, os.path.join(save_path_dir, "op_info.txt"))
+        dump_op_basic_info(op, os.path.join(save_path_dir, "op_info.txt"))
+
+        # stats json
+        stats_json_path = os.path.join(output_path, "stats.json")
+        if os.path.exists(stats_json_path):
+            with open(stats_json_path, "r") as f:
+                stats = json.load(f)
+        else:
+            stats = None
+        
+        result = ProfileResult(stats=stats, save_path=save_path_dir)
+        result_list.append(result)
+    return result_list
