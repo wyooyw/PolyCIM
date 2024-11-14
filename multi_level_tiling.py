f808116d304695a32e01f7fb46990d16ec6e7ea8
wangyiou <wangyiou12@163.com>
2024-11-14 06:16:48 -0800

    fix multilevel tiling

--- multi_level_tiling.py
+++ multi_level_tiling.py
@@ -24,7 +24,6 @@ def multi_level_tiling(operator, tiling_level, tiling_factors):
         [1,4]
     ]
     """
-    import pdb; pdb.set_trace()
     param_names = operator.domain.get_var_names(isl.dim_type.param)
 
     domain=operator.domain
@@ -70,7 +69,8 @@ def multi_level_tiling(operator, tiling_level, tiling_factors):
 
     tiling_map = tiling_map.intersect_domain(domain)
     
-    return operator.apply_schedule(tiling_map)
+    new_operator =  operator.apply_schedule(tiling_map)
+    return new_operator
 
 def factorize(N, T, depth=1, path=None, results=None):
     if path is None:
@@ -112,11 +112,11 @@ def enumerate_tiling_factors(operator, tiling_factor):
     dim_factors = []
     for dim_size in domain_shape:
         factors = factorize(dim_size, tiling_factor)
-        factors = filter_factors(factors)
-        # factors = [factor for factor in factors if factor[-1]!=1]
+        # factors = filter_factors(factors)
+        factors = [factor for factor in factors if factor[-1]!=1 or max(factor)==1]
         # print(f"{len(factors)=}, {factors=}")
         dim_factors.append(factors)
-
+    
     # exit()
     for combination in itertools.product(*dim_factors):
         new_operator = multi_level_tiling(operator, tiling_factor, combination)
@@ -131,4 +131,100 @@ def pre_tiling_pass(op_list):
     # new_op_list = new_op_list[:40]
     # print(len(new_op_list))
     # exit()
+    return new_op_list
+
+def multi_level_tiling_outer(operator, tiling_level, tiling_factors, inner_level=5):
+    """
+    operator = Operator(
+        domain=isl.BasicSet("{ [i,j]: 0 <= i < 8 and 0 <= j < 3 }"),
+        acc_rel_out=isl.BasicMap("{ [i,j] -> C[i] }"), 
+        acc_rel_lhs=isl.BasicMap("{ [i,j] -> A[i + j] }"), 
+        acc_rel_rhs=isl.BasicMap("{ [i,j] -> B[i + j] }")
+    )
+
+    tiling_factors: 
+    [
+        [2,2],
+        [1,4]
+    ]
+    """
+    param_names = operator.domain.get_var_names(isl.dim_type.param)
+
+    domain=operator.domain
+
+    assert domain.is_box(), f"domain={domain} is not box"
+    n_iter = domain.dim(isl.dim_type.set) - inner_level
+    assert len(tiling_factors)==n_iter, f"len(tiling_factors)={len(tiling_factors)} != n_iter={n_iter}"
+
+    domain_shape = utils.get_static_box_shape(domain)
+
+    # do checks
+    for i in range(n_iter):
+        assert len(tiling_factors[i])==tiling_level, f"len(tiling_factors[{i}])={len(tiling_factors[i])} != tiling_level={tiling_level}"
+        dim_size = domain_shape[i]
+        assert multiply(tiling_factors[i])==dim_size, f"multiply(tiling_factors[{i}])={multiply(tiling_factors[i])} != dim_size={dim_size}"
+
+    # do tiling
+    tiling_maps = []
+    remain_factors = [*domain_shape]
+    for l in range(tiling_level-1):
+        n_keep_iters = l * n_iter
+        keep_iter_names = [f"i{i}" for i in range(n_keep_iters)]
+        change_iter_names = [f"i{i}" for i in range(n_keep_iters, n_keep_iters+n_iter)]
+        inner_keep_iter_names = [f"i{i}" for i in range(n_keep_iters+n_iter, n_keep_iters+n_iter+inner_level)]
+
+        outer_iters = []
+        inner_iters = []
+        for i in range(n_iter):
+            factor = tiling_factors[i][l]
+            remain_factors[i] = remain_factors[i] // factor
+            outer_iters.append(f"floor({change_iter_names[i]}/{remain_factors[i]})")
+            inner_iters.append(f"{change_iter_names[i]}%{remain_factors[i]}")
+
+        tiling_map_def = f"[{','.join(param_names)}] -> {{ [{','.join(keep_iter_names + change_iter_names + inner_keep_iter_names)}] -> [{','.join(keep_iter_names + outer_iters + inner_iters + inner_keep_iter_names)}] }}"
+        tiling_map = isl.BasicMap(tiling_map_def)
+        # print(f"{tiling_map_def=}")
+        tiling_maps.append(tiling_map)
+
+   
+    # print("\n")
+    tiling_map = tiling_maps[0].intersect_domain(domain)
+    for _tiling_map in tiling_maps[1:]:
+        tiling_map = tiling_map.apply_range(_tiling_map)
+
+    tiling_map = tiling_map.intersect_domain(domain)
+    
+    new_operator =  operator.apply_schedule(tiling_map, skip_simplify=True)
+    return new_operator
+
+def enumerate_tiling_factors_outer(operator, tiling_factor, inner_level=5):
+    domain = operator.domain
+
+    assert domain.is_box(), f"domain={domain} is not box"
+    n_iter = domain.dim(isl.dim_type.set)
+
+    domain_shape = utils.get_static_box_shape(domain)[:n_iter-inner_level]
+    dim_factors = []
+    for dim_size in domain_shape:
+        factors = factorize(dim_size, tiling_factor)
+        # factors = filter_factors(factors)
+        factors = [factor for factor in factors if factor[-1]!=1 or max(factor)==1]
+        # print(f"{len(factors)=}, {factors=}")
+        if len(factors) > 8:
+            factors = factors[::4]
+        dim_factors.append(factors)
+    # import pdb; pdb.set_trace()
+    # exit()
+    # dim_factors = dim_factors[::4]
+    for combination in itertools.product(*dim_factors):
+        new_operator = multi_level_tiling_outer(operator, tiling_factor, combination, inner_level)
+        yield new_operator
+
+def memory_tiling_pass(op_list):
+    new_op_list = []
+    for op in op_list:
+        new_op_list.append(op)
+        for new_op in enumerate_tiling_factors_outer(op, 2, inner_level=5):
+            new_op_list.append(new_op)
+
     return new_op_list
