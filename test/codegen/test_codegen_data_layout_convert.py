f241d86eb00504aa60abf721188f21dddcbcec84
wangyiou <wangyiou12@163.com>
2025-01-17 11:24:26 +0800

    test: skewing data layout

--- test/codegen/test_codegen_data_layout_convert.py
+++ test/codegen/test_codegen_data_layout_convert.py
@@ -4,6 +4,7 @@ from codegen_.codegen_data_layout_convert import data_layout_convert
 import benchmark
 import utils
 import pytest
+from multi_level_tiling import multi_level_splitting_var_level
 
 def test_codegen_data_layout_convert_transpose():
     accrel_lhs = isl.Map("{ [i, j] -> A[i, j] : 0 <= i < 4 and 0 <= j < 4 }")
@@ -67,5 +68,53 @@ def test_codegen_data_layout_convert_im2col(
     output_data = data_layout_convert(accrel_input, accrel_output, input_data)
     assert np.allclose(output_data, im2col_input_golden_data), f"{output_data=}, {im2col_input_golden_data=}"
 
+def test_codegen_data_layout_convert_skewing():
+    out_size = 8
+    ker_size = 3
+    op = benchmark.get_op_dwconv2d(ic=1, oh=out_size, ow=out_size, kh=ker_size, kw=ker_size, stride=1, dilation=1, virtual_axis=False)
+
+    # pre-tiling
+    tiling_factors = [[1], [4,2], [4,2], [3], [3]]
+    op = multi_level_splitting_var_level(op, tiling_factors)
+
+    # skewing
+    skewing_schedule = isl.BasicMap(f"{{ [ic,ht,hp,wt,wp,kh,kw] -> [ic,ht,hp,wt,wp,kh+hp,kw+wp] }}")
+    op = op.apply_schedule(skewing_schedule)
+    
+    # coalescing
+    coalescing_schedule = isl.BasicMap(f"{{ [ic,ht,hp,wt,wp,u,v] -> [ic,ht,wt,row, col] : col = 2hp + wp and row = 4u + v }}")
+    reverse_schedule = isl.BasicMap(
+        f"{{ [ic,ht,wt,row, col] -> [ic,ht,hp,wt,wp,u,v] : hp = floor(col/2) and wp = col%2 and u = floor(row/4) and v = row%4 }}"
+    )
+    op = op.apply_schedule(coalescing_schedule, reverse_schedule=reverse_schedule, skip_simplify=True)
+
+    weight_data = np.arange(1,10).astype(np.int32).reshape(1,3,3)
+    accrel_input = op.access_W
+    accrel_output = isl.Map(f"{{ [ic,ht,wt,row,col] -> Wnew[ic,row,col] }}")
+    accrel_output = accrel_output.intersect_domain(op.domain)
+    output_data = data_layout_convert(accrel_input, accrel_output, weight_data)
+    golden_data = np.array(
+        [[
+            [1, 0, 0, 0],
+            [2, 1, 0, 0],
+            [3, 2, 0, 0],
+            [0, 3, 0, 0],
+            [4, 0, 1, 0],
+            [5, 4, 2, 1],
+            [6, 5, 3, 2],
+            [0, 6, 0, 3],
+            [7, 0, 4, 0],
+            [8, 7, 5, 4],
+            [9, 8, 6, 5],
+            [0, 9, 0, 6],
+            [0, 0, 7, 0],
+            [0, 0, 8, 7],
+            [0, 0, 9, 8],
+            [0, 0, 0, 9]
+        ]]
+    ).astype(np.int32)
+    assert np.allclose(output_data, golden_data), f"{output_data=}, {golden_data=}"
+
 if __name__ == "__main__":
-    test_codegen_data_layout_convert_im2col(1, 1, 1, 8, 3)
+    # test_codegen_data_layout_convert_im2col(1, 1, 1, 32, 3)
+    test_codegen_data_layout_convert_skewing()
