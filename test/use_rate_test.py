4849be3206f9909f87c28c596f29c29615144c12
wangyiou <wangyiou12@163.com>
2025-02-10 11:42:00 +0800

    fix

--- test/use_rate_test.py
+++ test/use_rate_test.py
@@ -20,7 +20,9 @@ def test_conv2d_2_8_8_3_3(cim_row_col_celluse):
         n_row=1,
         n_group_vcol=cim_col,
         n_comp=cim_row,
-        n_group=1
+        n_group=1,
+        n_macro_per_group=1,
+        n_macro=1
     )
     flops = operator.domain.count_val().get_num_si()
     op,cim_flops = run_pipeline(operator, skew=skew, cim_cfg=cim_cfg, save_dir=".temp_save")
@@ -49,7 +51,9 @@ def test_conv3d_8_8_8_3_3_3(cim_row_col_celluse
         n_row=1,
         n_group_vcol=cim_col,
         n_comp=cim_row,
-        n_group=1
+        n_group=1,
+        n_macro_per_group=1,
+        n_macro=1
     )
     flops = operator.domain.count_val().get_num_si()
     op,cim_flops = run_pipeline(operator, skew=skew, cim_cfg=cim_cfg, save_dir=".temp_save")
