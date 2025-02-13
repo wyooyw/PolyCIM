cefc9c2458e2e747d568334fc623e160d8d868cf
wangyiou <wangyiou12@163.com>
2025-02-13 16:12:28 +0800

    fix: scripts and configs

--- run_exp.sh
+++ run_exp.sh
@@ -1,12 +1,12 @@
 export CONFIG_PATH=$PWD/config/config.json
-export MAX_PROCESS_USE=1
+# export MAX_PROCESS_USE=1
 export PYTHONPATH=$PWD
 # export LOG_LEVEL="DEBUG"
 source log_config.sh
 
-# python -u polycim/cli/main.py explore \
-# --op-id test \
-# --config-path $PWD/test/explore/configs/g2m2c16b32.json \
-# --output-path .save
+python -u polycim/cli/main.py explore \
+--op-id C3 \
+--config-path $PWD/test/explore/configs/c32b64.json \
+--output-path .save
+# pytest -n 4 test/explore/test_result.py
 # python test/explore/test_result.py
-python test/explore/test_result.py
