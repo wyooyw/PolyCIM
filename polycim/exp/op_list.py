f25c8f5b91b6a7300ee5268ff467debf33887655
wangyiou <wangyiou12@163.com>
2025-02-13 13:57:28 +0800

    feat: set verify function in op_list

--- polycim/exp/op_list.py
+++ polycim/exp/op_list.py
@@ -1,5 +1,7 @@
 import polycim.op.benchmark as benchmark
 from collections import OrderedDict
+from polycim.op.calculate import depth_wise_conv2d
+from functools import partial
 def get_op_list():
 
     symmetry_info_for_dwconv2d = ((1,3),(2,4))
@@ -10,74 +12,95 @@ def get_op_list():
         "op": benchmark.get_op_dwconv2d(ic=1, oh=4, ow=4, kh=3, kw=3, stride=1, dilation=1, virtual_axis=False),
         "symmetry_info": symmetry_info_for_dwconv2d,
         "dim_types": dim_types_for_dwconv2d,
+        "verify_fn": depth_wise_conv2d,
+    }
+    op_list["d2h4"] = {
+        "op": benchmark.get_op_dwconv2d(ic=1, oh=4, ow=4, kh=3, kw=3, stride=1, dilation=2, virtual_axis=False),
+        "symmetry_info": symmetry_info_for_dwconv2d,
+        "dim_types": dim_types_for_dwconv2d,
+        "max_tiling_level": 3,
+        "verify_fn": partial(depth_wise_conv2d, dilation=2),
     }
     op_list["C1"] = {
         "op": benchmark.get_op_dwconv2d(ic=1, oh=112, ow=112, kh=3, kw=3, stride=1, dilation=1, virtual_axis=False),
         "symmetry_info": symmetry_info_for_dwconv2d,
         "dim_types": dim_types_for_dwconv2d,
+        "verify_fn": depth_wise_conv2d,
     }
     op_list["C2"] = {
         "op": benchmark.get_op_dwconv2d(ic=1, oh=56, ow=56, kh=3, kw=3, stride=1, dilation=1, virtual_axis=False),
         "symmetry_info": symmetry_info_for_dwconv2d,
         "dim_types": dim_types_for_dwconv2d,
+        "verify_fn": depth_wise_conv2d,
     }
     op_list["C3"] = {
         "op": benchmark.get_op_dwconv2d(ic=1, oh=28, ow=28, kh=5, kw=5, stride=1, dilation=1, virtual_axis=False),
         "symmetry_info": symmetry_info_for_dwconv2d,
         "dim_types": dim_types_for_dwconv2d,
+        "verify_fn": depth_wise_conv2d,
     }
     op_list["C4"] = {
         "op": benchmark.get_op_dwconv2d(ic=1, oh=14, ow=14, kh=3, kw=3, stride=1, dilation=1, virtual_axis=False),
         "symmetry_info": symmetry_info_for_dwconv2d,
         "dim_types": dim_types_for_dwconv2d,
+        "verify_fn": depth_wise_conv2d,
     }
     op_list["C5"] = {
         "op": benchmark.get_op_dwconv2d(ic=1, oh=14, ow=14, kh=5, kw=5, stride=1, dilation=1, virtual_axis=False),
         "symmetry_info": symmetry_info_for_dwconv2d,
         "dim_types": dim_types_for_dwconv2d,
+        "verify_fn": depth_wise_conv2d,
     }
     op_list["C6"] = {
         "op": benchmark.get_op_dwconv2d(ic=1, oh=7, ow=7, kh=5, kw=5, stride=1, dilation=1, virtual_axis=False),
         "symmetry_info": symmetry_info_for_dwconv2d,
         "dim_types": dim_types_for_dwconv2d,
+        "verify_fn": depth_wise_conv2d,
     }
     op_list["C7"] = {
         "op": benchmark.get_op_dwconv2d(ic=1, oh=7, ow=7, kh=3, kw=3, stride=1, dilation=1, virtual_axis=False),
         "symmetry_info": symmetry_info_for_dwconv2d,
         "dim_types": dim_types_for_dwconv2d,
+        "verify_fn": depth_wise_conv2d,
     }
     op_list["C8"] = {
         "op": benchmark.get_op_dwconv2d(ic=1, oh=56, ow=56, kh=7, kw=7, stride=1, dilation=1, virtual_axis=False),
         "symmetry_info": symmetry_info_for_dwconv2d,
         "dim_types": dim_types_for_dwconv2d,
+        "verify_fn": depth_wise_conv2d,
     }
     op_list["C9"] = {
         "op": benchmark.get_op_dwconv2d(ic=1, oh=8, ow=7, kh=7, kw=7, stride=1, dilation=1, virtual_axis=False),
         # "symmetry_info": symmetry_info_for_dwconv2d,
         "dim_types": dim_types_for_dwconv2d,
+        "verify_fn": depth_wise_conv2d,
     }
     op_list["C10"] = {
         "op": benchmark.get_op_dwconv2d(ic=1, oh=56, ow=56, kh=51, kw=51, stride=1, dilation=1, virtual_axis=False),
         "symmetry_info": symmetry_info_for_dwconv2d,
         "dim_types": dim_types_for_dwconv2d,
-        "not_tiling": [3,4]
+        "not_tiling": [3,4],
+        "verify_fn": depth_wise_conv2d,
     }
     op_list["C11"] = {
         "op": benchmark.get_op_dwconv2d(ic=1, oh=7, ow=7, kh=13, kw=13, stride=1, dilation=1, virtual_axis=False),
         "symmetry_info": symmetry_info_for_dwconv2d,
-        "dim_types": dim_types_for_dwconv2d
+        "dim_types": dim_types_for_dwconv2d,
+        "verify_fn": depth_wise_conv2d,
     }
     op_list["C12"] = {
         "op": benchmark.get_op_dwconv2d(ic=1, oh=28, ow=28, kh=3, kw=3, stride=1, dilation=2, virtual_axis=False),
         "symmetry_info": symmetry_info_for_dwconv2d,
         "dim_types": dim_types_for_dwconv2d,
-        "max_tiling_level": 3
+        "max_tiling_level": 3,
+        "verify_fn": partial(depth_wise_conv2d, dilation=2),
     }
     op_list["C13"] = {
         "op": benchmark.get_op_dwconv2d(ic=1, oh=28, ow=28, kh=5, kw=5, stride=1, dilation=2, virtual_axis=False),
         "symmetry_info": symmetry_info_for_dwconv2d,
         "dim_types": dim_types_for_dwconv2d,
-        "max_tiling_level": 3
+        "max_tiling_level": 3,
+        "verify_fn": partial(depth_wise_conv2d, dilation=2),
     }
     # # op_list["C14"] = benchmark.get_op_dwconv2d(b=1, oc=1, ic=1, oh=28, ow=28, kh=5, kw=5, stride=1, dilation=2)
     op_list["C15"] = {
--- polycim/op/calculate.py
+++ polycim/op/calculate.py
@@ -1,19 +1,28 @@
 import numpy as np
 
-def depth_wise_conv2d(input, weight):
+def depth_wise_conv2d(input, weight, dilation=1, stride=1):
     assert len(input.shape) == 3
     assert len(weight.shape) == 3
     ic, ih, iw = input.shape
     kc, kh, kw = weight.shape
     assert ic == kc
-    oh = ih - kh + 1
-    ow = iw - kw + 1
+    
+    effective_kh = kh + (kh - 1) * (dilation - 1)
+    effective_kw = kw + (kw - 1) * (dilation - 1)
+    oh = (ih - effective_kh) // stride + 1
+    ow = (iw - effective_kw) // stride + 1
     oc = ic
     output = np.zeros((oc, oh, ow), dtype=input.dtype)
     # for _oc in range(oc):
     for _oh in range(oh):
         for _ow in range(ow):
-            input_window = input[:, _oh: _oh + kh, _ow: _ow + kw]
+            input_window = input[:, _oh: _oh + kh * dilation: dilation, _ow : _ow + kw * dilation : dilation]
             weight_window = weight[:, :, :]
             output[:, _oh, _ow] += np.sum(input_window * weight_window, axis=(1,2))
     return output
+
+if __name__ == "__main__":
+    input = np.arange(64).reshape(1, 8, 8).astype(np.int32)
+    weight = np.arange(9).reshape(1, 3, 3).astype(np.int32)
+    output = depth_wise_conv2d(input, weight, dilation=2)
+    print(output)
